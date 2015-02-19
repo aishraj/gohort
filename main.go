617641aefd6e4ad841c9ca7b87a832914514a712
Aishraj <aishraj@users.noreply.github.com>
2015-02-19 06:08:24 +0530

    Fixing Flags in the sample runner

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	gohort "github.com/aishraj/gohort/shortner"
+	"github.com/aishraj/gohort/shortner"
 	"log"
 	"runtime"
 	"strconv"
@@ -14,16 +14,18 @@ func main() {
 	redisPortInt := flag.Int("rport", 6379, "Port on which Redis is running")
 	redisTimeOutSeconds := flag.Int("timeout", 10, "Timeout for Redis connection in seconds")
 	serverPortInt := flag.Int("sport", 8080, "Port for the HTTP server")
-
-	serverPort := strconv.Itoa(*serverPortInt)
-	redisPort := strconv.Itoa(*redisPortInt)
 	cpus := flag.Int("cpus", runtime.NumCPU(), "Number of CPUs to use")
 
 	flag.Parse()
+
+	serverPort := strconv.Itoa(*serverPortInt)
+	redisPort := strconv.Itoa(*redisPortInt)
+
 	runtime.GOMAXPROCS(*cpus)
 
 	log.Printf("Starting the server with properties. Redis host %s "+
 		"Redis port number %s Redis Timeout seconds %d HTTP Server port %s",
 		*redisHost, redisPort, *redisTimeOutSeconds, serverPort)
-	gohort.RegisterAndStart(*redisHost, redisPort, serverPort, *redisTimeOutSeconds)
+
+	shortner.RegisterAndStart(*redisHost, redisPort, serverPort, *redisTimeOutSeconds)
 }
